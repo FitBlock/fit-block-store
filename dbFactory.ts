a04cf8e32a9bf57c0857dbe11688e8b0d733d27d
zy445566 <zyct@vip.qq.com>
2020-01-10 15:03:28 +0800

    增加推导泛型

--- dbFactory.ts
+++ dbFactory.ts
@@ -1,18 +1,21 @@
-import Level from './db/level'
 import dbBase from './types/dbBase'
+import Level from './db/level'
 const instanceMap: Map<String, any> = new Map(); 
 export default class dbFactory {
-    static getDBInstance(name:string):dbBase {
-        if(instanceMap.has(name)) {
-            return instanceMap.get(name);
-        }
+    static getDBInstance(name:string) {
         switch(name.toLowerCase()) {
             case 'level':
-                instanceMap.set(name, new Level());
-                break;
+                return dbFactory.getDBByClass<Level>(Level);
             default:
                 throw new Error('not support db.')
         }
-        return instanceMap.get(name);
+    }
+    static getDBByClass<T extends dbBase>(db: new () => T):T {
+        if(instanceMap.has(db.name)) {
+            return instanceMap.get(db.name);
+        }
+        const dbInstance = new db()
+        instanceMap.set(db.name, dbInstance)
+        return dbInstance;
     }
 }
